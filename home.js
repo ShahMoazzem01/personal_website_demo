2dbe313fd39d5b947d54b43cd9d8482e6c346003
shahmoazzem <shahmoazzem01@gmail.com>
2023-03-18 23:18:19 +0600

    demo complete, all button of home working, no signIn and register

--- home.js
+++ home.js
@@ -1,15 +1,64 @@
-const pic = document.querySelector('.pic');
-const showPic = document.getElementById('showPic');
-let show = true;
+// const pic = document.querySelector('.pic');
+// const showPic = document.getElementById('showPic');
+// let show = true;
 
-const togglePic = (showPicture) =>{
-    showPicture? pic.style.display = 'none' : pic.style.display = 'flex';
+// const togglePic = (showPicture) =>{
+//     showPicture? pic.style.display = 'none' : pic.style.display = 'flex';
+// }
+
+// console.log(show)
+// showPic.addEventListener('click', e=>{
+//     togglePic(show);
+//     show = !show;
+//     console.log(show);
+// })
+
+const home = document.querySelector(".home");
+const cv = document.querySelector(".cv");
+const pic = document.querySelector(".pic");
+const link = document.querySelector(".link");
+
+const btnHome = document.getElementById("liHome");
+const btnCv = document.getElementById("liCv");
+const btnPic = document.getElementById("liPic");
+const btnLink = document.getElementById("liLink");
+
+const show = true;
+
+const showHome = (show) =>{
+    show? home.style.display = 'block' : home.style.display = 'none';
+}
+const showCv = (show) =>{
+    show? cv.style.display = 'block' : cv.style.display = 'none';
+}
+const showPic = (show) =>{
+    show?  pic.style.display = 'flex' : pic.style.display = 'none';
+}
+const showLink = (show) =>{
+    show? link.style.display = 'block' : link.style.display = 'none';
 }
 
-console.log(show)
-showPic.addEventListener('click', e=>{
-    togglePic(show);
-    show = !show;
-    console.log(show);
-})
-
+btnHome.addEventListener('click',(e)=>{
+    showHome(show);
+    showCv(!show);
+    showPic(!show);
+    showLink(!show);
+});
+btnCv.addEventListener('click',(e)=>{
+    showHome(!show);
+    showCv(show);
+    showPic(!show);
+    showLink(!show);
+});
+btnPic.addEventListener('click',(e)=>{
+    showHome(!show);
+    showCv(!show);
+    showPic(show);
+    showLink(!show);
+});
+btnLink.addEventListener('click',(e)=>{
+    showHome(!show);
+    showCv(!show);
+    showPic(!show);
+    showLink(show);
+});
